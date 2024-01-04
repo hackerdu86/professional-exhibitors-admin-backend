cb49c09bf54795c7e324f3c3d49e104870a71192
Youva Chaou <youvachaou1234@gmail.com>
2024-01-04 18:16:39 -0500

    sa

--- controllers/exhibitor-controller.js
+++ controllers/exhibitor-controller.js
@@ -3,15 +3,66 @@ const Exhibitor = require("../models/exhibitor");
 
 //Get
 async function getExhibitors(req, res, next) {
-    
+  let exhibitors = [];
+  try {
+    exhibitors = await Exhibitor.find({});
+    res.json({
+      exhibitors: exhibitors.map((exhibitor) => {
+        return exhibitor.toObject({ getters: true });
+      }),
+    });
+  } catch (err) {
+    console.log(err);
+    new HttpError("Error while trying to fetch exhibitors", 500);
+  }
 }
-async function getExhibitor(req, res, next) {}
+async function getExhibitor(req, res, next) {
+  const { id } = req.params;
+  let exhibitor;
+  try {
+    exhibitor = await Exhibitor.findById(id);
+    res.status(201).json({
+      exhibitor: exhibitor,
+    });
+  } catch (err) {
+    console.log(err);
+    return next(new HttpError("Error while retrieving exhibitor", 500));
+  }
+}
 
 //Post
-async function addExhibitor(req, res, next) {}
+async function addExhibitor(req, res, next) {
+  const { firstName, lastName, phoneNumber, picturePath } = req.body;
+  try {
+    const exhibitorToAdd = new Exhibitor({
+      firstName,
+      lastName,
+      phoneNumber,
+      picturePath,
+    });
+    await exhibitorToAdd.save();
+    res
+      .status(201)
+      .json({ exhibitor: exhibitorToAdd.toObject({ getters: true }) });
+  } catch (err) {
+    console.log(err);
+    return next(new HttpError("Error while creating exhibitor", 500));
+  }
+}
 
 //Delete
-async function deleteExhibitor(req, res, next) {}
+async function deleteExhibitor(req, res, next) {
+  const id = req.params.internshipId;
+  try {
+    await Exhibitor.deleteOne({ _id: id });
+    res.status(200).json({
+      message: "Exhibitor was successfully removed",
+    });
+  } catch (err) {
+    console.log(err);
+    return next(new HttpError("Error while trying to delete exhibitor", 500));
+  }
+}
 
 module.exports = {
   getExhibitors: getExhibitors,
--- models/exhibitor.js
+++ models/exhibitor.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const exhibitorSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  picturePath: { type: String, required: true },
 });
-
 module.exports = mongoose.model("Exhibitor", exhibitorSchema);
